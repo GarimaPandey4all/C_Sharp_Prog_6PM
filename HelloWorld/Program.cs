e55c41d92858bce54accca35dcdea313ebf6350d
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-01 21:33:53 +0530

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) // main is the entry of every C# program
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
