d0b727c853bb3e23a00429a5d57d56149ef92750
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-03 19:56:50 +0530

    Add files via upload

---
+++ Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿//using System; // using- keyword/Reserved Word, System- Namespace: It is a collection of classes.
+
+/*
+class: category/layout/blueprint/logical entity
+e.g: Human - brain, action, eyes, nose, name, age, etc */
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //Main(): Entry point or initial point of any C# Program
+        //void: return type of main(), void means null or empty
+        //string[] args: string - data type , [] - subcript operator/Array, args - name of the variable
+        static void Main(string[] args)
+        {
+            //Console- class
+            //WriteLine() - method
+            Console.WriteLine("Hello World!"); // print output on the output screen
+        }
+    }
+}
