0472572db9e1e1bc8908d1ecb69efcd548b952e9
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-05 19:13:37 +0530

    Add files via upload

---
+++ Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50, b = 100;
+
+            Console.WriteLine("Addition is: "+(a + b)); // + : Concatenation/joining
+        }
+    }
+}
---
+++ VariableAndConstant/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariableAndConstant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; // Declaration/Create and Initialization/Assignment/Definition
+
+            int c = a + 30;
+
+            Console.WriteLine(c);
+
+            c = 60;
+
+            Console.WriteLine(c);
+
+            const float pi = 3.14f; // const : constant : fixed value, value can't be changed
+
+            pi = 56.7f;
+
+            Console.WriteLine(pi);
+        }
+    }
+}
