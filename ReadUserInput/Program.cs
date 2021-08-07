4ca9f8d0d994c45dd686cde37edd15b65aded0e4
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-07 19:04:48 +0530

    Add files via upload

---
+++ ReadUserInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReadUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Age");
+            int age = Convert.ToInt32(Console.ReadLine()); // Console.ReadLine() - Read User Input
+
+            Console.WriteLine("Your age is "+age);
+        }
+    }
+}
