ec7a3a0e05858ecd0470d78f495436eff833f39d
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-09 19:12:35 +0530

    Add files via upload

---
+++ AssignmentOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssignmentOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            a += 30; // a = a + 30; // shorthand
+            a *= 50;
+            a %= 100;
+            a <<= 45;
+        }
+    }
+}
---
+++ BitwiseOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BitwiseOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4, b = 5;
+
+            Console.WriteLine(a & b);
+            Console.WriteLine(a | b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(a << 1);
+            Console.WriteLine(a >> 1);
+        }
+    }
+}
