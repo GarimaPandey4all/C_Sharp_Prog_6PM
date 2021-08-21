ad515d809452d28e78f7e779344f28024511171e
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-21 13:10:34 +0530

    Add files via upload

---
+++ Array-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array: It is a collection of similar types of data. It contains multiple values in single variable.
+
+            int[] marks = {10, 20, 30, 40, 50};
+
+            Console.WriteLine(marks[0]);
+            Console.WriteLine(marks[1]);
+            Console.WriteLine(marks[2]);
+            Console.WriteLine(marks[4]);
+
+            for(int i = 0; i < marks.Length; i++)
+            {
+                Console.Write(marks[i] + " ");
+            }
+
+            Console.WriteLine("\n" +marks.Length);
+
+
+        }
+    }
+}
---
+++ Continue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                if(i == 5) // true
+                {   
+                    i++; // i = 5 + 1 = 6
+                    continue; // i = 6 = skip
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
