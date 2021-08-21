459e2c6b6ee38229bda6c4a46278ad490dc9c703
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-21 13:12:08 +0530

    Add files via upload

---
+++ Array-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 9, 12, 2, 0, 1, 18, 17, 100};
+
+            Array.Sort(arr);
+
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Min());
+            Console.WriteLine(arr.Sum());
+        }
+    }
+}
---
+++ Array-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            Console.WriteLine("Enter Values");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Values in Array are");
+            foreach(int i in numbers)
+            {
+                Console.Write(i+" ");
+            }
+            
+        }
+    }
+}
